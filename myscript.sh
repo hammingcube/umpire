10737aab9e42cf10fb2c6cee9693776d2d16d860
maddyonline <madhavjha@gmail.com>
2017-04-09 00:26:49 -0400

    advanced build

---
+++ myscript.sh
@@ -0,0 +1,5 @@
+rm -rf files/clean_dir
+mkdir -p files/clean_dir
+git clone https://github.com/maddyonline/optcode-secrets files/clean_dir/optcode-secrets
+git clone https://github.com/maddyonline/problemset files/clean_dir/problemset
+
